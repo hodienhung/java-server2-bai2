f1d2dd84fdfc05969bc0743aa43bbc7a72cb9806
hodienhung <lynguyen20013@gmail.com>
2026-07-30 17:43:13 +0700

    Create TaskController.java

---
+++ TaskController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Task;
+import com.example.demo.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.findAllTasks();
+    }
+}
